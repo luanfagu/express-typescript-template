7d1b6d124318f664fd98d4e4ab84cfe72d0cfa15
luan <luan@turris.dev>
2020-11-10 22:11:10 -0300

    Created the User interface and model

---
+++ src/models/User/User.ts
@@ -0,0 +1,29 @@
+import { IUser } from '../../interfaces/User/IUser'
+import mongoose from 'mongoose';
+
+const User = new mongoose.Schema(
+    {
+        name: {
+            type: String
+            , required: true
+            , index: true
+        }
+        , email: {
+            type: String
+            , unique: true
+            , required: true
+            , index: true
+        }
+        , password: {
+            type: String
+            , required: true
+        }
+        , role: {
+            type: String
+            , default: 'user'
+        }
+    }
+    , { timestamps: true }
+);
+
+export default mongoose.model<IUser & mongoose.Document>('User', User);
